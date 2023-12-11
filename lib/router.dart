5797e27b8af64f5208986895738bdd128f1d2375
leo <peirolo@yahoo.fr>
2023-12-11 17:21:51 +0100

    fin de création des models pour liste parking, en train de préparer appels api

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,12 @@
-  class AppRouter{
+  import 'package:park_in_angers/ui/screens/liste_parking.dart';
 
+class AppRouter{
+    // static const String homePage = '/home';
+    static const String listeParking = '/liste_parking';
+
+    static final routes = {
+      // homePage: (context) => const Home(),
+      listeParking: (context) => ListeParking(),
+
+    };
 }
---
+++ lib/ui/screens/liste_parking.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListeParking extends StatelessWidget {
+  const ListeParking({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Liste des parking'),
+        ),
+        body: Container()
+    );
+
+  }
+}
