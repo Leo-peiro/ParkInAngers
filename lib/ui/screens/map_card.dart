674b6004ff2ea2a4d4d6ce49944d8266f0ab2417
Sixtine <sixtine.mignot@reseau.eseo.fr>
2024-01-07 23:23:49 +0100

    feat[slide]: Slide bar pour voir plus d'informations sur le parking sélectionné

--- lib/ui/screens/map_card.dart
+++ lib/ui/screens/map_card.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
-
-import '../../models/adresse.dart';
+import 'package:we_slide/we_slide.dart';
 
 class MapCard extends StatefulWidget {
   @override
@@ -18,7 +16,8 @@ class _MapCardState extends State<MapCard> {
     AdresseMarker("Avenue Montaigne", LatLng(47.4642, -0.5511)),
   ];
 
-  late AdresseMarker _favoriteParking = AdresseMarker("Rue de la Roë", LatLng(47.4697, -0.5560));
+  late AdresseMarker _favoriteParking =
+  AdresseMarker("Rue de la Roë", LatLng(47.4697, -0.5560));
 
   void setFavoriteParking(AdresseMarker favoriteParking) {
     setState(() {
@@ -28,104 +27,103 @@ class _MapCardState extends State<MapCard> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(47.4666700, -0.5500000),
-        zoom: 10.6,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
-          subdomains: const ['a', 'b', 'c'],
-          keepBuffer: 20,
-          tileProvider: NetworkTileProvider(),
-        ),
-        MarkerLayer(
-          markers: adressesAngers
-              .asMap()
-              .entries
-              .map((entry) => Marker(
-            point: entry.value.position,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  for (var other in adressesAngers) {
-                    other.isSelected = false;
-                  }
-                  entry.value.isSelected = !entry.value.isSelected;
+    final _colorScheme = Theme.of(context).colorScheme;
+    final double _panelMinSize = 70.0;
+    final double _panelMaxSize = MediaQuery.of(context).size.height / 2;
 
-                  if (_favoriteParking == entry.value || !_favoriteParking.isSelected) {
-                    setFavoriteParking(entry.value);
-                  }
-                });
-              showDialog(
-                context: context,
-                builder: (context) {
-                return AlertDialog(
-                  title: Text("Nom de la rue"),
-                  content: Text(entry.value.name),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                      Navigator.of(context).pop();
-                      },
-                      child: Text('Fermer'),
-                   ),
-                ],
-                );
-              },
-              );
-              },
-              child: AnimatedContainer(
-                alignment : Alignment.center,
-                duration: const Duration(milliseconds: 500),
-                child: Icon(
-                  size: 45.0,
-                  Icons.location_pin,
-                  color: entry.value.isSelected
-                      ? Colors.lightBlueAccent
-                      : Colors.red,
-                ),
-              ),
-            ),
-          ))
-              .toList(),
-        ),
-
-        CurrentLocationLayer(),
-        Stack(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: WeSlide(
+        panelMinSize: _panelMinSize,
+        panelMaxSize: _panelMaxSize,
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(47.4666700, -0.5500000),
+            zoom: 10.6,
+          ),
           children: [
-            // Autres widgets de votre page
-
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                margin: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12.0),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 10.0,
-                      offset: Offset(0.0, 10.0),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    // Display favorite parking information
-                    Text(_favoriteParking.name),
-                    Text(_favoriteParking.position.toString()),
-                  ],
-                ),
-              ),
+            TileLayer(
+              urlTemplate:
+              'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
+              subdomains: const ['a', 'b', 'c'],
+              keepBuffer: 20,
+              tileProvider: NetworkTileProvider(),
             ),
+            MarkerLayer(
+              markers: adressesAngers
+                  .asMap()
+                  .entries
+                  .map((entry) => Marker(
+                point: entry.value.position,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      for (var other in adressesAngers) {
+                        other.isSelected = false;
+                      }
+                      entry.value.isSelected = !entry.value.isSelected;
+
+                      if (_favoriteParking == entry.value ||
+                          !_favoriteParking.isSelected) {
+                        setFavoriteParking(entry.value);
+                      }
+                    });
+
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Nom de la rue"),
+                          content: Text(entry.value.name),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Fermer'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: AnimatedContainer(
+                    alignment: Alignment.center,
+                    duration: const Duration(milliseconds: 500),
+                    child: Icon(
+                      size: 45.0,
+                      Icons.location_pin,
+                      color: entry.value.isSelected
+                          ? Colors.lightBlueAccent
+                          : Colors.red,
+                    ),
+                  ),
+                ),
+              ))
+                  .toList(),
+            ),
+            CurrentLocationLayer(),
           ],
-        )
-      ],
+        ),
+        panel: Container(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Information de votre Parking Favoris"),
+                Text(_favoriteParking.name),
+                Text(_favoriteParking.position.toString()),
+              ],
+            ),
+          ),
+        ),
+        panelHeader: Container(
+          height: _panelMinSize,
+          color: Colors.white,
+          child: Center(child: Text("Faire glisser vers le haut pour des informations!️")),
+        ),
+      ),
     );
   }
 }
